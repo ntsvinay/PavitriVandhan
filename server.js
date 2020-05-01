3f2e46a2b7f6e4ada3ab196691dea271e7427ce6
vinay mohniya <ntsvinay@gmail.com>
2020-05-01 18:04:01 +0530

    uploading the project_5

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/dist/pavitribandhan'));
+app.use(express.static(__dirname + '/pavitribandhan'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/pavitribandhan/index.html'));
+    res.sendFile(path.join(__dirname + '/pavitribandhan/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
