114d0e4bcd99659337a9bfd14b340ad7db5d8a61
vinay mohniya <ntsvinay@gmail.com>
2020-05-01 15:03:51 +0530

    uploading the project_4

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/<pavitribandhan>'));
+app.use(express.static(__dirname + '/dist/Pavitribandhan'));
 
 app.get('/*', function(req, res) {
 
-    res.sendFile(path.join(__dirname + '/dist/<pavitribandhan>/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Pavitribandhan/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
