50d12e7059374f2966443525bed000b02cb4efd5
vinay mohniya <ntsvinay@gmail.com>
2020-05-02 21:37:55 +0530

    size fix update star and fix header

--- src/app/matched-profile/matched-profile.component.ts
+++ src/app/matched-profile/matched-profile.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-matched-profile',
@@ -9,13 +9,33 @@ import {HttpClient} from '@angular/common/http';
 export class MatchedProfileComponent implements OnInit {
 
   apiData;
-  apiUrl='https://jsonplaceholder.typicode.com/users';
-  constructor( private http:HttpClient) { }
+  apiUrl = 'https://jsonplaceholder.typicode.com/users';
+  constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    this.http.get(this.apiUrl).subscribe((data)=>{console.warn(data)
-    this.apiData=data;}
+    this.http.get(this.apiUrl).subscribe((data) => {
+      console.warn(data)
+      this.apiData = data;
+    }
     )
+  }
+  public shortList_userId=[];
+  public icon = 'assets/images/icon_1.png';
+  public icon1 = 'assets/images/icon_2.png';
+  public btnId=0; 
+
+  shortlist(val) {
+    if(!this.shortList_userId.includes((val))){
+    this.shortList_userId.push(val);
+    
+    }
+    else{
+      const index: number = this.shortList_userId.indexOf(val);
+      if(index!=-1){
+      this.shortList_userId.splice(index,1);
+     
+      }
+    }
   }
 
 }
