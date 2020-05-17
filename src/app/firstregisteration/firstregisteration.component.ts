d5e775aef9a796528faa1d923ed7127ce5f5f074
vinay mohniya <ntsvinay@gmail.com>
2020-05-17 15:39:19 +0530

    email error and all

--- src/app/firstregisteration/firstregisteration.component.ts
+++ src/app/firstregisteration/firstregisteration.component.ts
@@ -1,8 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpErrorResponse,
+  HttpHandler,
+  HttpEvent,
+  HttpResponse
+} from '@angular/common/http';
+
+import { Observable, EMPTY, throwError, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { ok } from 'assert';
 
 @Component({
   selector: 'app-firstregisteration',
@@ -16,45 +28,75 @@ export class FirstregisterationComponent implements OnInit {
   registerationDetails: FormGroup;
   castes;
   religionList;
+  motherTongueList;
+  profileOwnerList;
+  registrationApiReturnData;
 
   saveRegistrationDetails = 'http://localhost:8080/userDetails';
-  casteApi = 'https://pavitrivandhanapi.herokuapp.com/caste/';
-  religionApi = 'https://pavitrivandhanapi.herokuapp.com/allReligion';
-  // casteApi = 'http://localhost:8080/caste/';
-  // religionApi = 'http://localhost:8080/allReligion';
+  // casteApi = 'https://pavitrivandhanapi.herokuapp.com/caste/';
+  // religionApi = 'https://pavitrivandhanapi.herokuapp.com/allReligion';
+  casteApi = 'http://localhost:8080/caste/';
+  religionApi = 'http://localhost:8080/allReligion';
+  profileOwnerApi = 'http://localhost:8080/profileOwner';
+  mothertongueApi = 'http://localhost:8080/motherTongue';
 
   castelist(valueReligions) {
 
     this.casteApi = this.casteApi;
-    alert(this.casteApi)
-    this.http.get(this.casteApi + valueReligions).subscribe((data) => {
+        this.http.get(this.casteApi + valueReligions).subscribe((data) => {
       console.warn(data)
       this.castes = data;
     })
 
   }
+get reg(){return this.registerationDetails.controls}
+  onRegistrationSubmitError=false;
+
   //submiting the login details
   onRegisterDetailsSubmit() {
-
-    alert(this.registerationDetails.value.userName + this.registerationDetails.value.userEmail + this.registerationDetails.value.userGender + this.registerationDetails.value.userMotherTongue
-
-      + this.registerationDetails.value.userProfileOwner + this.registerationDetails.value.religion_id + this.registerationDetails.value.caste_id);
-
-    if (this.registerationDetails.value.userName == '') {
-      this.router.navigate(['/firstregisteration']);
-
+    this.onRegistrationSubmitError=true;
+    if (this.registerationDetails.invalid) {
+      return;
     }
-    else {
+    let basicSetup = {
+      userName: this.registerationDetails.value.userName,
+      userEmail: this.registerationDetails.value.userEmail,
+      userGender: this.registerationDetails.value.userGender,
+      // userMotherTongue: this.registerationDetails.value.userMotherTongue,
+      // userProfileOwner: this.registerationDetails.value.userProfileOwner,
+      userCaste: {
+        "id": this.registerationDetails.value.userCaste,
+        caste_id: this.registerationDetails.value.userCaste,
+        caste_religion_id: this.registerationDetails.value.religion_id
 
+      }, "userReligion": {
+        "id": this.registerationDetails.value.religion_id,
+        religion_id: this.registerationDetails.value.religion_id
 
+      },"userMotherTongue": {
+        "motherTongueId":this.registerationDetails.value.userMotherTongue
+      },"userProfileOwner":{"profileCreaterId":this.registerationDetails.value.userProfileOwner}
+    }
+    this.registerationDetails = this.formBuilder.group(basicSetup);
+    
       console.warn(this.registerationDetails);
-      this.http.post(this.saveRegistrationDetails, this.registerationDetails.value).subscribe((data: any[]) => {
+       this.http.post(this.saveRegistrationDetails, this.registerationDetails.value).subscribe
+       (data=>(this.router.navigate(['/preferences'])),error=>(this.onRegistrationSubmitError=true));
+       
+      //   this.registrationApiReturnData = res;
+      // }
+      
+      // ,(err:HttpErrorResponse)=>{
+      //   this.onRegistrationSubmitError=true;      
+      // });
+      
+      // this.router.navigate(['/preferences']);
+     
+      
+      //  this.router.navigate(['/inforamtionpage']);
+    
 
-        console.warn(data)
-
-      })
-      // this.router.navigate(['/inforamtionpage']);
-    }
+  
   }
 
 
@@ -89,7 +131,7 @@ export class FirstregisterationComponent implements OnInit {
       userMotherTongue: ['-Select-', [Validators.required]],
       userProfileOwner: ['-Select-', [Validators.required]],
       religion_id: ['-Select', [Validators.required]],
-      caste_id: ['-Select-', [Validators.required]]
+      userCaste: ['-Select-', [Validators.required]]
 
     })
 
@@ -97,8 +139,21 @@ export class FirstregisterationComponent implements OnInit {
     this.http.get(this.religionApi).subscribe((data) => {
       console.warn(data)
       this.religionList = data
-      this.religionList
+    
     })
+
+    //calling mother tongue api
+    this.http.get(this.mothertongueApi).subscribe((data) => {
+      console.warn(data)
+      this.motherTongueList = data
+     
+    })
+
+    this.http.get(this.profileOwnerApi).subscribe((data) => {
+      console.warn(data)
+      this.profileOwnerList = data
+    })
+    
   }
   // convenience getter for easy access to form fields
   get f() { return this.registerForm.controls; }
